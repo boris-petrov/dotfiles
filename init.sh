317319ae8dee05b20beef375825708e92ac1718d
Boris Petrov <boris_petrov@live.com>
2012-02-08 23:56:40 +0200

    Added FARR stuff

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ SCRIPTPATH=$(dirname $SCRIPT)
 if [ `uname -s` = "Linux" ]; then
 	for file in $SCRIPTPATH/*
 	do
-		if [ $file != "$SCRIPTPATH/init.sh" -a $file != "$SCRIPTPATH/README" -a $file != "$SCRIPTPATH/.git" -a $file != "$SCRIPTPATH/.minttyrc" -a $file != "$SCRIPTPATH/AutoHotkey.ahk" -a $file != "$SCRIPTPATH/prio.ini" -a $file != "$SCRIPTPATH/_vsvimrc"  ]
+		if [ $file != "$SCRIPTPATH/init.sh" -a $file != "$SCRIPTPATH/README" -a $file != "$SCRIPTPATH/.git" -a $file != "$SCRIPTPATH/.minttyrc" -a $file != "$SCRIPTPATH/AutoHotkey.ahk" -a $file != "$SCRIPTPATH/prio.ini" -a $file != "$SCRIPTPATH/_vsvimrc" -a $file != "$SCRIPTPATH/DonationCoder" ]
 		then
 			ln -sf $file ~/$(basename $file)
 		fi
@@ -39,6 +39,12 @@ elif [ `uname -o` = "Cygwin" ]; then
 		elif [ $file == "$SCRIPTPATH/_vsvimrc" ]; then
 			cmd /c del %USERPROFILE%\\_vsvimrc
 			cmd /c mklink %USERPROFILE%\\_vsvimrc $(cygpath -w $SCRIPTPATH/_vsvimrc)
+		elif [ $file == "$SCRIPTPATH/FindAndRunRobot.ini" ]; then
+			cmd /c del %USERPROFILE%\\Documents\\DonationCoder\\FindAndRunRobot\\FindAndRunRobot.ini
+			cmd /c mklink %USERPROFILE%\\Documents\\DonationCoder\\FindAndRunRobot\\FindAndRunRobot.ini $(cygpath -w $SCRIPTPATH/FindAndRunRobot.ini)
+		elif [ $file == "$SCRIPTPATH/myaliases.alias" ]; then
+			cmd /c del %USERPROFILE%\\Documents\\DonationCoder\\FindAndRunRobot\\AliasGroups\\MyCustom\\myaliases.alias
+			cmd /c mklink %USERPROFILE%\\Documents\\DonationCoder\\FindAndRunRobot\\AliasGroups\\MyCustom\\myaliases.alias $(cygpath -w $SCRIPTPATH/myaliases.alias)
 		elif [ $file != "$SCRIPTPATH/init.sh" -a $file != "$SCRIPTPATH/README" -a $file != "$SCRIPTPATH/.git" ]; then
 			ln -sf $file ~/$(basename $file)
 		fi
