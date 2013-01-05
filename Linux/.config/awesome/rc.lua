64fb5f58737b8450d0314e35cc265651e8c70500
Boris Petrov <boris_petrov@live.com>
2013-01-05 14:53:34 +0200

    [awesome] Add rule for Flash player so it shows correctly in full-screen

--- Linux/.config/awesome/rc.lua
+++ Linux/.config/awesome/rc.lua
@@ -270,6 +270,7 @@ awful.rules.rules = {
       properties = { tag = tags[1][2], switchtotag = true } },
     { rule = { class = "Firefox", instance = "Dialog", name = "Password Required" },
       properties = { switchtotag = false } },
+    { rule = { class = "Plugin-container" }, properties = { floating = true } }, -- Flash player
     -- { rule = { class = "Firefox", instance = "Dialog" },
     --   callback = move_to_current_tag },
     { rule = { class = "Skype", name = "File Transfers" },
