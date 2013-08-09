9f755bf5b1e339d83a42760375c3b7b1dcf282bc
Boris Petrov <boris_petrov@live.com>
2013-08-09 18:36:51 +0300

    [Linux] Use urxvtd and start urxvt clients from awesome

--- Linux/.config/awesome/rc.lua
+++ Linux/.config/awesome/rc.lua
@@ -58,7 +58,7 @@ end
 -- Themes define colours, icons, and wallpapers
 beautiful.init(awful.util.getdir("config") .. "/themes/default/theme.lua")
 
-terminal         = "urxvt"
+terminal         = "urxvtc"
 hostile_takeover = "gvim /home/boris/documents/Hostile\\ Takeover.txt"
 
 alt_modkey = "Mod1"
