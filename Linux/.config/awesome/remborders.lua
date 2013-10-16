8049f436565bdd3dc5279532d64c83ae1e602a1c
Boris Petrov <boris_petrov@live.com>
2013-10-16 18:24:32 +0300

    [awesome] Update remborders script

--- Linux/.config/awesome/remborders.lua
+++ Linux/.config/awesome/remborders.lua
@@ -1,46 +1,18 @@
 local awful     = require("awful")
 local beautiful = require("beautiful")
 
-local function client_maximized(c)
-    if c.maximized_horizontally and c.maximized_vertically then
-        return true
-    end
+for s = 1, screen.count() do
+    screen[s]:connect_signal("arrange", function()
+        local clients = awful.client.visible(s)
+        local layout  = awful.layout.getname(awful.layout.get(s))
 
-    local s_geom = screen[c.screen].geometry
-    local c_geom = c:geometry()
-
-    if (s_geom.x == c_geom.x and s_geom.y == c_geom.y
-
-        and s_geom.width == c_geom.width and s_geom.height == c_geom.height) then
-
-        return true
-    else
-        return false
-    end
-end
-
-local function border_width(screen_index, client)
-    if awful.client.floating.get(client) then
-        if client_maximized(client) then
-            return 0
-        else
-            return beautiful.border_width
-        end
-    end
-
-    if (#awful.client.tiled(screen_index) == 1 or
-        awful.layout.get(screen_index) == awful.layout.suit.max) then
-        return 0
-    else
-        return beautiful.border_width
-    end
-end
-
--- remove borders if only one window is visible
-for i = 1, screen.count() do
-    screen[i]:connect_signal("arrange", function(s)
-        for _, c in ipairs(awful.client.visible(i)) do
-            c.border_width = border_width(i, c)
+        for _, c in ipairs(clients) do
+            -- No borders with only one visible client
+            if #clients == 1 or layout == "max" then
+                c.border_width = 0
+            else
+                c.border_width = beautiful.border_width
+            end
         end
     end)
 end
