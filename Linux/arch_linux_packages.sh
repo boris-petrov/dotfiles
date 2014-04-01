c30df72aff10c61256245249b1c7f0957bfa4c0b
Boris Petrov <boris_petrov@live.com>
2014-04-01 17:18:08 +0300

    [Linux] More updates to Arch Linux packages

--- Linux/arch_linux_packages.sh
+++ Linux/arch_linux_packages.sh
@@ -1,30 +1,81 @@
 #!/bin/bash
 
+cd
+
+# install packer
 wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
 tar -xzf packer.tar.gz
-cd packer
+pushd packer
 makepkg -s
 pacman -U packer-*
+popd
+rm -rf packer packer.tar.gz
 
+# for infinality packages
+sudo pacman-key -r 962DDE58
+sudo pacman-key --lsign-key 962DDE58
+
+# refresh repositories
+sudo pacman -Syy
+
+# install packages
 packer -S archlinux-keyring
 packer -S xf86-video-intel xf86-input-synaptics
 packer -S zathura feh libreoffice smplayer kbgoffice deadbeef
 packer -S cups gutenprint system-config-printer
 packer -S networkmanager network-manager-applet
 packer -S anything-sync-daemon profile-sync-daemon chromium-pepper-flash firefox
-packer -S pidgin skype skype4pidgin kbdd-latest-git pidgin-otr purple-whatsapp
-packer -S dropbox liferea xcmenu-git thunderbird hotot htop autokey-gtk arandr xdg-utils
-packer -S xorg-server xorg-xinit awesome dmenu xlockmore
+packer -S pidgin skype skype4pidgin-svn-dbus kbdd-latest-git pidgin-otr purple-whatsapp
+packer -S dropbox liferea xcmenu-git thunderbird hotot-qt4 htop autokey-gtk arandr xdg-utils
+packer -S xorg-server xorg-xinit slim awesome dmenu xlockmore
 packer -S aspell hunspell
 packer -S gvim colordiff colorgcc git ctags the_silver_searcher
 packer -S zsh rxvt-unicode-pixbuf urxvt-clipboard tmux
-packer -S jre ntp openssh sudo wget ntfs-3g xsel unrar oxygen-icons
+packer -S jre ntp openssh sudo wget ntfs-3g xsel oxygen-icons
+packer -S zip unrar unzip
 packer -S deluge flareget
 packer -S freerdp realvnc-viewer
 packer -S python2-pip python2-howdoi
 packer -S infinality-bundle infinality-bundle-multilib ibfonts-meta-base otf-inconsolatazi4-ibx
+packer -S nodejs
 
+# install howdoi dependencies
 sudo pip2 install requests-cache
 sudo pip2 install pygments
 
-# https://wiki.archlinux.org/index.php/Infinality-bundle%2Bfonts
+# install npm
+wget https://www.npmjs.org/install.sh
+sudo sh install.sh
+rm install.sh
+sudo npm install -g coffee-script
+
+# clone my dotfiles, install them and install screenful
+git clone git@github.com:boris-petrov/dotfiles.git
+pushd dotfiles
+./init.sh
+pushd Linux/.config/awesome
+./install-screenful.sh
+popd
+popd
+
+# install ruby's bundler
+sudo gem install bundler
+
+# install vrome
+mkdir -p code/vrome
+pushd code/vrome
+git clone git@github.com:jinzhu/vrome.git
+sudo bundle
+rake build
+popd
+
+# add less keybindings
+lesskey
+
+# enable services
+sudo systemctl enable slim
+sudo systemctl enable sshd
+sudo systemctl enable ntpd
+sudo systemctl enable NetworkManager
+
+# should install aspell and hunspell dictionaries from Dropbox directory
