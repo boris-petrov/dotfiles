1ea85afde996f30409ef61d76f52fac11dc45446
Boris Petrov <boris_petrov@live.com>
2019-11-20 10:04:45 +0200

    [prepare_system.sh] Install the correct `autokey` package

--- Linux/prepare_system.sh
+++ Linux/prepare_system.sh
@@ -48,7 +48,7 @@ yay -S archlinux-keyring \
   networkmanager network-manager-applet networkmanager-pptp networkmanager-openvpn networkmanager-l2tp gnome-keyring \
   anything-sync-daemon profile-sync-daemon profile-cleaner chromium pepper-flash firefox flashplugin \
   pidgin telegram-purple-git purple-skypeweb-git purple-hangouts-hg purple-facebook-git slack-libpurple-git purple-discord-git kbdd-git pidgin-otr \
-  dropbox pcloud liferea xcmenu-git thunderbird htop autokey-py3 xdg-utils lxappearance feh gnome-themes-standard \
+  dropbox pcloud liferea xcmenu-git thunderbird htop autokey xdg-utils lxappearance feh gnome-themes-standard \
   xorg-server xorg-xinit slim awesome dmenu xorg-xprop xscreensaver arandr \
   aspell hunspell \
   gvim colordiff universal-ctags-git the_silver_searcher ripgrep fzf grc ngrok ncdu \
